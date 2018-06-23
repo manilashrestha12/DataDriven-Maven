c4b4722c67ce0696985d6a6c6a194374b134b29e
just_ <just_@DESKTOP-1B0EOJN>
2018-06-23 11:53:45 -0500

    commiting to Data Driven And MAven

---
+++ DataDrivenMavenPOM/src/util/BrowserFactory.java
@@ -0,0 +1,50 @@
+package util;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class BrowserFactory {
+	
+	static WebDriver driver;
+	
+	public static WebDriver startBrowser(String browserName, String url){
+		if(browserName.equalsIgnoreCase("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver", "../WordpressDemo/drivers/geckodriver.exe");
+			
+			ProfilesIni profile = new ProfilesIni();
+			FirefoxProfile myProfile = profile.getProfile("default");
+			DesiredCapabilities dc = DesiredCapabilities.firefox();
+			dc.setCapability(FirefoxDriver.PROFILE, myProfile);
+			dc.setCapability("marionette", true);
+			
+			driver = new FirefoxDriver(dc);
+			
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+		
+		else if(browserName.equalsIgnoreCase("chrome"))
+		{
+			//System.setProperty("webdriver.chrome.driver", "C:/auto1/chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver.exe");
+
+
+			driver = new ChromeDriver();
+		}
+		
+		driver.get(url);
+		
+		return driver;
+	}
+	
+	public void closeBrowser(){
+		driver.close();
+	}
+
+}
